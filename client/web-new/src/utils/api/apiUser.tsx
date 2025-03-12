540cc3780e453bc871586a68e504a73322a4a6d5
0rtemk <sarte@Kavbanga.ru>
2025-03-12 19:30:56 +0300

    feat/add-new-client-version

---
+++ client/web-new/src/utils/api/apiUser.tsx
@@ -0,0 +1,79 @@
+import axios from "axios";
+import Cookies from "universal-cookie";
+
+export const cookies = new Cookies();
+
+const API_BASE_URL = import.meta.env.VITE_API_USER_URL;
+console.log(import.meta.env);
+
+const getAPIClient = axios.create({
+  baseURL: `${API_BASE_URL}/users`,
+});
+
+getAPIClient.interceptors.request.use(
+  (config) => {
+    const token = cookies.get("access_token");
+    console.log("Token from cookies:", token);
+    if (token) {
+      config.headers.Authorization = `Bearer ${token}`;
+      console.log("Authorization header added:", config.headers.Authorization);
+    }
+    return config;
+  },
+  (error) => {
+    return Promise.reject(error);
+  }
+);
+
+export const API_USER = {
+  login: async ({ username, password }) => {
+    try {
+      const sendData = {
+        username: username,
+        password: password,
+      };
+      const answer = await axios.post(
+        `${API_BASE_URL}/users/signin/`,
+        sendData,
+        { withCredentials: true }
+      );
+      console.log(answer.data);
+      return answer;
+    } catch (error) {
+      console.log(error);
+    }
+  },
+
+  logout: async () => {
+    const sendData = {
+      refresh: cookies.get("refresh_token"),
+    };
+    const answer = await getAPIClient.post("/logout/", sendData, {
+      withCredentials: true,
+    });
+    return answer;
+  },
+
+  register: async ({ email, username, password, role = "student" }) => {
+    const sendData = {
+      email,
+      username,
+      password,
+      role,
+    };
+    const answer = await axios.post(`${API_BASE_URL}/users/signup/`, sendData, {
+      withCredentials: true,
+    });
+    return answer;
+  },
+
+  me: async () => {
+    try {
+      const answer = await getAPIClient.get("/me/");
+      return answer.data;
+    } catch (error) {
+      console.log(error);
+      throw error;
+    }
+  },
+};
